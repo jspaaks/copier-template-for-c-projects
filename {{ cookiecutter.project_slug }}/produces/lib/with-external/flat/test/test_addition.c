0df778fae2372aeed516e2b3e94346249b07100b
Jurriaan H. Spaaks <jhspaaks@hotmail.com>
2024-07-18 13:48:49 -0400

    added test directory for lib/with-external/flat

---
+++ {{ cookiecutter.project_slug }}/produces/lib/with-external/flat/test/test_addition.c
@@ -0,0 +1,15 @@
+#include <criterion/criterion.h>
+#include "addition.h"
+
+
+Test(addition, one){
+    int actual = add(2, 3);
+    int expected = 5;
+    cr_assert(actual == expected);
+}
+
+Test(addition, two){
+    int actual = add(2, -3);
+    int expected = -1;
+    cr_assert(actual == expected);
+}
