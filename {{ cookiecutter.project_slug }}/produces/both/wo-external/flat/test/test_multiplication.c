c245f49cefbf97000b29fe34ff12c98e41ae790f
Jurriaan H. Spaaks <jhspaaks@hotmail.com>
2024-07-18 13:36:07 -0400

    added test directory for both/with-external/flat

---
+++ {{ cookiecutter.project_slug }}/produces/both/wo-external/flat/test/test_multiplication.c
@@ -0,0 +1,15 @@
+#include <criterion/criterion.h>
+#include "multiplication.h"
+
+
+Test(multiplication, one){
+    int actual = multiply(2, 3);
+    int expected = 6;
+    cr_assert(actual == expected);
+}
+
+Test(multiplication, two){
+    int actual = multiply(2, -3);
+    int expected = -6;
+    cr_assert(actual == expected);
+}
