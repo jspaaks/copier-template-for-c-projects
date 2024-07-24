59bbdb119b75c86fddb16750cd8e2fceefd8b046
Jurriaan H. Spaaks <jhspaaks@hotmail.com>
2024-07-24 12:18:40 -0400

    fuzzy testing proof of concept

--- fuzzy/conftest.py
+++
@@ -1,6 +0,0 @@
-def pytest_addoption(parser):
-    parser.addoption("--all", action="store_true", help="run all combinations")
-
-
-def pytest_generate_tests(metafunc):
-    metafunc.parametrize("generated", range(10))
--- fuzzy/test_template.py
+++ fuzzy/test_template.py
@@ -1,21 +1,22 @@
 import pytest
 import subprocess
 import yaml
-import itertools
 from pathlib import Path
+import itertools
+import os
 import random
 
 
-def make_parameterization(keys, values):
+def get_parameterization(keys, values):
     d = {key: value for (key, value) in zip(keys, values)}
     ident = ",".join(["{}={}".format(key, value) for (key, value) in zip(keys, values)])
     marks = [
-        pytest.mark.skip("producesexe and produceslib are both False") if d["producesexe"] == False and d["produceslib"] == False else None,
+        pytest.mark.skip("producesexe and produceslib are both False") if d["producesexe"] == False and d["produceslib"] == False else None
     ]
     return pytest.param(d, id=ident, marks=[mark for mark in marks if mark is not None])
 
 
-def get_params():
+def get_parameterizations():
     with open("copier.yml", "r") as fid:
         copier_config = yaml.safe_load(fid)
     boolset = {True, False}
@@ -37,12 +38,21 @@ def get_params():
         "projectname": ["calculator-project", "navigator-project"]
     }
     valuess = itertools.product(*data.values())
-    valuess_shuffled = valuess
-    return [make_parameterization(data.keys(), values) for values in valuess_shuffled]
+    tmp = os.environ.get("NFUZZY", None)
+    nfuzzy = None if tmp is None else int(tmp)
+    assert nfuzzy is not None, "These tests need NFUZZY environment variable"
+    valuess_sampled = random.sample(list(valuess), nfuzzy)
+    return [get_parameterization(data.keys(), values) for values in valuess_sampled]
 
 
-@pytest.fixture(scope="module", params=get_params())
+@pytest.fixture(scope="module", params=get_parameterizations())
 def generated(tmp_path_factory, request):
+    """
+    This fixture's parameterization depends on the environment
+    variable NFUZZY being present and containing a (string that
+    can be parsed into a) valid integer.
+    """
+
     defaults = {
         "add_assets": True,
         "add_clang_format": True,
@@ -64,26 +74,38 @@ def generated(tmp_path_factory, request):
     answers.update(request.param)
 
     basedir = tmp_path_factory.mktemp("base")
-
     inputdir = basedir / "input"
     inputdir.mkdir()
-
     outputdir = basedir / "output"
     outputdir.mkdir()
 
     data_file = inputdir / "data.yml"
     with open(data_file, "wt") as fid:
         yaml.dump(answers, fid)
-    subprocess.run(["copier", "copy", "--data-file", data_file, ".", outputdir ])
+    subprocess.run(["copier", "copy", "--data-file", data_file, "--vcs-ref=HEAD", ".", outputdir ])
     return {
         "answers": answers,
         "directory": outputdir
     }
 
 
+def meets_expected_presence(*, ispresent=True, directories=None, files=None):
+    if directories is not None:
+        for d in directories:
+            assert True if d is None else ispresent == d.is_dir(), d
+    if files is not None:
+        for f in files:
+            assert True if f is None else ispresent == f.is_file(), f
+    return True
+
+def get_answers(answers, *keys):
+    return [ answers[key] for key in keys ]
+
+
 def test_assets_generation(generated):
-    add_assets = generated["answers"]["add_assets"]
-    base = generated["directory"] / generated["answers"]["projectname"]
+    add_assets, projectname = \
+            get_answers(generated["answers"], "add_assets", "projectname")
+    base = generated["directory"] / projectname
     directories = [
         base / "assets",
         base / "assets" / "fonts",
@@ -95,33 +117,60 @@ def test_assets_generation(generated):
         base / "assets" / "images" / "image.bmp",
         base / "assets" / "sounds" / "sound.wav"
     ]
-    for d in directories:
-        assert add_assets == d.is_dir()
-    for f in files:
-        assert add_assets == f.is_file()
+    assert meets_expected_presence(ispresent=add_assets, directories=directories, files=files)
 
 
 def test_clang_format_generation(generated):
-    add_clang_format = generated["answers"]["add_clang_format"]
-    base = generated["directory"] / generated["answers"]["projectname"]
+    add_clang_format, projectname = \
+            get_answers(generated["answers"], "add_clang_format", "projectname")
+    base = generated["directory"] / projectname
     files = [
         base / ".clang-format"
     ]
-    for f in files:
-        assert add_clang_format == f.is_file()
+    assert meets_expected_presence(ispresent=add_clang_format, files=files)
+
+def test_cmake_generation(generated):
+    add_assets, add_cmake, add_external, add_test, build_directory, exename, external_directory, libname, nested, producesexe, produceslib, projectname = get_answers(generated["answers"], "add_assets", "add_cmake", "add_external", "add_test", "build_directory", "exename", "external_directory", "libname", "nested", "producesexe", "produceslib", "projectname")
+    base = generated["directory"] / projectname
+    directories = [ base / build_directory / "cmake" ]
+    files = [ base / "CMakeLists.txt" ]
+    if producesexe:
+        if nested:
+            files += [ base / "src" / exename / "CMakeLists.txt"]
+        else:
+            files += [ base / "src" / "CMakeLists.txt" ]
+    if produceslib:
+        if nested:
+            files += [ base / "src" / libname / "CMakeLists.txt" ]
+            if add_test:
+                files += [ base / "test" / libname / "CMakeLists.txt" ]
+        else:
+            files += [ base / "src" / "CMakeLists.txt" ]
+            if add_test:
+                files += [ base / "test" / "CMakeLists.txt" ]
+    if add_assets:
+            files += [
+                base / "assets" / "CMakeLists.txt",
+                base / "assets" / "fonts" / "CMakeLists.txt",
+                base / "assets" / "images" / "CMakeLists.txt",
+                base / "assets" / "sounds" / "CMakeLists.txt"
+            ]
+    if add_external:
+        files += [
+            base / external_directory / "CMakeLists.txt",
+        ]
+    assert meets_expected_presence(ispresent=add_cmake, directories=directories, files=files)
 
 
 def test_codeblocks_generation(generated):
-    add_codeblocks = generated["answers"]["add_codeblocks"]
-    base = generated["directory"] / generated["answers"]["projectname"]
+    add_codeblocks, projectname, build_directory = \
+            get_answers(generated["answers"], "add_codeblocks", "projectname", "build_directory")
+    base = generated["directory"] / projectname
     directories = [
-        base / generated["answers"]["build_directory"] / "codeblocks"
+        base / build_directory / "codeblocks"
     ]
     files = [
         base / ".codeblocks" / "project.cbp",
         base / ".codeblocks" / "project.layout"
     ]
-    for d in directories:
-        assert add_codeblocks == d.is_dir()
-    for f in files:
-        assert add_codeblocks == f.is_file()
+    assert meets_expected_presence(ispresent=add_codeblocks, directories=directories, files=files)
