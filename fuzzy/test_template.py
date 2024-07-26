9a9cd9144d621397e4a0c0daa39530421391085f
Jurriaan H. Spaaks <jhspaaks@hotmail.com>
2024-07-26 11:56:41 -0400

    testing the generated tests on macos in ci

--- fuzzy/test_template.py
+++ fuzzy/test_template.py
@@ -12,6 +12,7 @@ def get_parameterization(keys, values):
     ident = ",".join(["{}={}".format(key, value) for (key, value) in zip(keys, values)])
     marks = [
         pytest.mark.skip("producesexe and produceslib are both False") if d["producesexe"] == False and d["produceslib"] == False else None
+        pytest.markskip("temporary subsetting") if not (d["produceslib"] and d["producesexe"] and d["nested"] == False and d["add_external"]) else None
     ]
     return pytest.param(d, id=ident, marks=[mark for mark in marks if mark is not None])
 
